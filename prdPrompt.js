682ea74f8ef637fdfc5bd9a92115718b781dc2dc
happyboy1327-sketch <happyboy1327@gmail.com>
2026-08-12 08:54:34 +0900

    Update model ID to 'mistral-large-latest'

--- prdPrompt.js
+++ prdPrompt.js
@@ -1,4 +1,4 @@
-const MODEL_ID = "mistral-nemo-2407";
+const MODEL_ID = "mistral-large-latest";
 
 const PRD_SYSTEM_PROMPT = `
 You are an expert system for generating accurate Korean-language general knowledge quizzes.
