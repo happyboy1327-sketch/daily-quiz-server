6fdaaf873703feb117ff13e1f8e5df0569b13506
happyboy1327-sketch <happyboy1327@gmail.com>
2026-09-13 09:44:57 +0900

    Modify createQuizPayload to include ANSWER_HISTORY
    Added ANSWER_HISTORY parameter to createQuizPayload function and updated prompt instructions.

--- prdPrompt.js
+++ prdPrompt.js
@@ -411,7 +411,7 @@ const FEW_SHOT_DATABASE = {
   ]
 };
 
-function createQuizPayload(topic, spellingData = null) {
+function createQuizPayload(topic, spellingData = null, ANSWER_HISTORY) {
 
   let systemPrompt = PRD_SYSTEM_PROMPT;
 
@@ -506,7 +506,7 @@ ${topic}
 
 위 분야에 맞는 중급 난도의 퀴즈 1개를 JSON 형식으로 출제해주세요.
 이미 출제된 목록과 동일하거나 유사한 문제는 절대 출제하지 말고, 다양한 세부 주제와 사실을 선택하십시오.
-동일한 개념이나 정답을 같은 topic에 넣어 반복하지 마십시오. 또한, systemPrompt와 FEW_SHOT_DATABASE에서 다뤄지지 않은 새로운 소재를 사용하십시오.
+ANSWER_HISTORY 속 동일한 개념이나 정답을 같은 topic에 넣어 반복하지 마십시오. 또한, systemPrompt와 FEW_SHOT_DATABASE에서 다뤄지지 않은 새로운 소재를 사용하십시오.
 
 `
 }
