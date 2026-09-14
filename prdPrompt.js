a5755f5e74337d23fd4257f6a7a05f91470f6fc2
happyboy1327-sketch <happyboy1327@gmail.com>
2026-09-14 20:34:55 +0900

    Limit previous questions displayed to last 10 items

--- prdPrompt.js
+++ prdPrompt.js
@@ -537,8 +537,8 @@ ${topic}
 ANSWER_HISTORY 속 동일한 개념이나 정답을 같은 topic에 넣어 반복하지 마십시오. 또한, systemPrompt와 FEW_SHOT_DATABASE에서 다뤄지지 않은 새로운 소재를 사용하십시오.
 
 출제 식별값: ${randomOID}-${randomDirective}
-이전 문제 목록: 이전 세트 질문:
-${previousQuestions.slice(-18).join("\n")}
+이전 문제 목록: 
+${previousQuestions.slice(-10).join("\n")}
 `
 }
     ],
