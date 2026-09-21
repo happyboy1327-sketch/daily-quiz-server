d016c227f6db7e6f773db6e2fffae85f13e37559
happyboy1327-sketch <happyboy1327@gmail.com>
2026-09-21 20:51:37 +0900

    Adjust temperature and frequency penalties in prdPrompt.js

--- prdPrompt.js
+++ prdPrompt.js
@@ -557,9 +557,9 @@ ${previousQuestions.slice(-14).map(q =>
 `
 }
     ],
-    temperature: 0.14, 
+    temperature: 0.2, 
     presence_penalty: 0.83,
-    frequency_penalty: 1.15,
+    frequency_penalty: 0.95,
     max_tokens: 2150
   };
 }
