d1d4c75e865bf426fd9062f6a944f9e2fadd0961
happyboy1327-sketch <happyboy1327@gmail.com>
2026-09-21 21:39:51 +0900

    Increase timeout from 40s to 50s in server.js

--- server.js
+++ server.js
@@ -1594,7 +1594,7 @@ async function fetchNewQuizData(requiredCount = 5, excludedQuestions = [], exclu
                             'Authorization': `Bearer ${UPSTAGE_API_KEY}`,
                             'Content-Type': 'application/json'
                         },
-                        timeout: 40000
+                        timeout: 50000
                     },
                     2,
                     2500
